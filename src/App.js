bd79c8da7e466b8ac923943df647461a314c1ade
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-04-29 18:06:32 -0500

    made a basic function when pressed button console logs hello in react

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import House from "./app/views/house";
 import Home from "./app/views/home";
 
 function App() {
+
+    const sayHello = () => {
+        console.log('hello');
+    }
+
   return (
     <div>
         <h1>Hello React</h1>
+        <button onClick={sayHello}>Hello</button>
     </div>
   );
 }
