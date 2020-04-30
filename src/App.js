4b8e4eeda0a6b2964cdfe34b9e63a4de771a9cd5
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-04-29 19:23:58 -0500

    added some basic js to react to change a counter and color of html

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import Home from "./app/views/home";
 import Tweet from "./app/views/Tweet"
 
 function App() {
 
+    const [isRed, setRed] = useState(false)
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+        setRed(!isRed);
+    }
+
   return (
     <div className="app">
-        <Tweet name="Michael" message="Hello" likes="50"/>
-        <Tweet name="David" message="Goodbye" likes="30"/>
-        <Tweet name="John" message="My name is John" likes="100"/>
-        <Tweet name="Bob" message="Im bob" likes="1"/>
+        <h1 className={isRed ? 'red' : ''}>Change my color</h1>
+        <button onClick={increment}>Increment</button>
+        <h1>{count}</h1>
     </div>
   );
 }
