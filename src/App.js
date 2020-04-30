ccf1463fc172d2d2e8ae4fe3d475ffe75e3fd19c
Michael McDonnell <Michael.McDonnell067@gmail.com>
2020-04-29 19:31:16 -0500

    created a basic state and used it in a component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,32 @@ import Tweet from "./app/views/Tweet"
 
 function App() {
 
-    const [isRed, setRed] = useState(false)
-    const [count, setCount] = useState(0)
+    const [users, setUsers] = useState([
+        {name: 'Ed', message: 'Hello there'},
+        {name: 'John', message: 'I am John snow'},
+        {name: 'travesery', message: 'I am awesome'}
+    ])
 
-    const increment = () => {
-        setCount(count + 1)
-        setRed(!isRed);
-    }
+    // const [isRed, setRed] = useState(false)
+    // const [count, setCount] = useState(0)
+    //
+    // const increment = () => {
+    //     setCount(count + 1)
+    //     setRed(!isRed);
+    // }
 
   return (
     <div className="app">
-        <h1 className={isRed ? 'red' : ''}>Change my color</h1>
-        <button onClick={increment}>Increment</button>
-        <h1>{count}</h1>
+
+        {users.map(user => (
+            <Tweet name={user.name} message={user.message}/>
+        ))}
+
+
+
+        {/*<h1 className={isRed ? 'red' : ''}>Change my color</h1>*/}
+        {/*<button onClick={increment}>Increment</button>*/}
+        {/*<h1>{count}</h1>*/}
     </div>
   );
 }
